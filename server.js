85e50823cc6584b86896726520228149410de534
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-07 10:18:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,35 +9,53 @@ app.use(cors());
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "*"
-  }
+    cors: {
+        origin: "*"
+    }
 });
 
 let players = {};
 
 io.on("connection", (socket) => {
-  console.log("player conectado:", socket.id);
 
-  players[socket.id] = { x: 100, y: 100 };
+    console.log("Jogador conectado:", socket.id);
 
-  io.emit("players", players);
-
-  socket.on("move", (data) => {
-    if (players[socket.id]) {
-      players[socket.id].x = data.x;
-      players[socket.id].y = data.y;
-    }
+    players[socket.id] = {
+        x: 0,
+        y: 0,
+        z: 0,
+        rx: 0,
+        ry: 0,
+        rz: 0
+    };
 
     io.emit("players", players);
-  });
 
-  socket.on("disconnect", () => {
-    delete players[socket.id];
-    io.emit("players", players);
-  });
+    socket.on("updatePlayer", (data) => {
+
+        players[socket.id] = {
+            x: data.x,
+            y: data.y,
+            z: data.z,
+            rx: data.rx,
+            ry: data.ry,
+            rz: data.rz
+        };
+
+        io.emit("players", players);
+    });
+
+    socket.on("disconnect", () => {
+
+        delete players[socket.id];
+
+        io.emit("players", players);
+
+        console.log("Jogador saiu:", socket.id);
+    });
+
 });
 
 server.listen(process.env.PORT || 3000, () => {
-  console.log("server rodando");
+    console.log("Servidor online");
 });
