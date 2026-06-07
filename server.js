25ca7d37b93c16a41000547dcf2a31968c6f25f3
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-07 13:38:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ const wss = new WebSocket.Server({ server });
 
 let players = {};
 
+// ---------------------------
+// BROADCAST
+// ---------------------------
 function broadcast() {
     const data = JSON.stringify({
         type: "players",
@@ -26,6 +29,27 @@ function broadcast() {
     });
 }
 
+// ---------------------------
+// CLEANUP (HEARTBEAT SYSTEM)
+// ---------------------------
+setInterval(() => {
+    const now = Date.now();
+
+    for (const id in players) {
+        if (!players[id].lastSeen) continue;
+
+        if (now - players[id].lastSeen > 5000) {
+            console.log("Removendo player inativo:", id);
+            delete players[id];
+        }
+    }
+
+    broadcast();
+}, 2000);
+
+// ---------------------------
+// CONNECTION
+// ---------------------------
 wss.on("connection", (ws) => {
 
     const id = Math.random().toString(36).substr(2, 9);
@@ -34,9 +58,11 @@ wss.on("connection", (ws) => {
 
     players[id] = {
         x: 0, y: 0, z: 0,
-        rx: 0, ry: 0, rz: 0
+        rx: 0, ry: 0, rz: 0,
+        lastSeen: Date.now()
     };
 
+    // envia init
     ws.send(JSON.stringify({
         type: "init",
         id: id,
@@ -45,24 +71,39 @@ wss.on("connection", (ws) => {
 
     broadcast();
 
+    // ---------------------------
+    // MESSAGE
+    // ---------------------------
     ws.on("message", (msg) => {
         try {
             const data = JSON.parse(msg);
 
             if (data.type === "update") {
-                players[id] = data.data;
+
+                players[id] = {
+                    ...data.data,
+                    lastSeen: Date.now()
+                };
+
                 broadcast();
             }
 
         } catch (e) {}
     });
 
+    // ---------------------------
+    // CLOSE (backup only)
+    // ---------------------------
     ws.on("close", () => {
+        console.log("Player desconectado:", id);
         delete players[id];
         broadcast();
     });
 });
 
+// ---------------------------
+// START SERVER
+// ---------------------------
 server.listen(process.env.PORT || 3000, () => {
     console.log("Servidor online");
 });
