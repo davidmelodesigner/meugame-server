3c0ebb2c30b760a37c656a4f9a7cedc2c9693220
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-08 14:54:39 -0300

    Update loginserver.js

--- loginserver.js
+++ loginserver.js
@@ -2,6 +2,9 @@ module.exports = function loginserver(ws, data) {
     console.log("CONECTING USERS");
 
     ws.send(JSON.stringify({
-        message: "userconected"
+        message: "userconected",
+        email: data.email,
+        password:data.password
+        
     }));
 };
