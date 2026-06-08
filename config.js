5257a85b466a263417ba675a5b4f5c02add956f6
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-08 15:45:22 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,10 @@
 module.exports = function callconfigs(types) {
-  
+  switch(types){
+
+    case 'postgre':
+      return "postgresql://neondb_owner:npg_qUTQ3o4esZjF@ep-sparkling-pond-apv9ip8u-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require";
+
+    default:
+      return null;
+  }
 };
