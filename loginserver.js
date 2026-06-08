f12765bf3b9dee1325dcf2580d7acf14091f5d4a
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-08 16:47:33 -0300

    Update loginserver.js

--- loginserver.js
+++ loginserver.js
@@ -1,49 +1,65 @@
 const { Pool } = require("pg");
-const callconfigs = require("./config");
-
 const crypto = require("crypto");
 
-function createUserId(socketId, email) {
+const callconfigs = require("./config");
+
+function createUserId(email) {
     const emailMd5 = crypto
         .createHash("md5")
         .update(email.toLowerCase())
         .digest("hex");
 
-    return `${socketId}_${emailMd5}`;
+    const sessionId = crypto.randomUUID();
+
+    return `${sessionId}_${emailMd5}`;
 }
 
 const pool = new Pool({
-    connectionString: callconfigs('postgre'),
+    connectionString: callconfigs("postgre"),
     ssl: { rejectUnauthorized: false }
 });
 
 module.exports = function loginserver(ws, data) {
-    console.log("CONECTING USERS");
+
+    console.log("CONNECTING USER:", data.email);
 
     async function checkLogin(email, password) {
+
         try {
+
             const result = await pool.query(
                 "SELECT * FROM users WHERE email = $1 AND password = $2",
                 [email, password]
             );
 
             if (result.rows.length > 0) {
-                const socketId = crypto.randomUUID();
 
-                const userId = createUserId(socketId, data.email);
-                
+                const userId = createUserId(email);
+
                 ws.send(JSON.stringify({
                     message: "userconnected",
-                    userid: userId
+                    userid: userId,
+                    email: email
                 }));
+
             } else {
+
+                console.log("LOGIN FAILED");
+
                 ws.send(JSON.stringify({
                     message: "loginfailed"
                 }));
+
             }
 
         } catch (err) {
+
             console.log("Erro login:", err);
+
+            ws.send(JSON.stringify({
+                message: "servererror"
+            }));
+
         }
     }
 
