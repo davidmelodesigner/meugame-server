837c611f88c52317a540c59281d9329bf00caee2
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-09 16:35:45 -0300

    Update connectserver.js

--- connectserver.js
+++ connectserver.js
@@ -1,18 +1,6 @@
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
 module.exports = function connectserver(req, res) {
-    wss.on("connection", (ws) => {
-
-            ws.on("message", (msg) => {
-                const data = JSON.parse(msg.toString());
-                if (data.message == "startserver") {
-                    inicServer(ws, data);
-                }
-            });
-        
-            
-        
-        });
+    ws.send(JSON.stringify({
+                message: "serverconnected"
+            }));
 
 };
