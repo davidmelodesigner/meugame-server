c4e077768c39a819e0de7769b07cadaa3ab90a5a
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-09 12:24:56 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const http = require("http");
-const WebSocket = require("ws");
 
 const logout = require("./logout");
 const loginserver = require("./loginserver");
@@ -11,12 +10,16 @@ const homepage = require("./home");
 const connectserver = require("./connectserver");
 
 const app = express();
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
 
 app.get("/", (req, res) => {
     homepage(req, res);
 });
+
+app.get("/inicserver", (req, res) => {
+    inicServer(req, res);
+});
+
+const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000, () => {
     console.log("Servidor online");
