c753be679acf0451724e87e8566d5cd6f5082fa2
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-08 21:25:58 -0300

    Update inicServer.js

--- inicServer.js
+++ inicServer.js
@@ -1,8 +1,11 @@
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
 
-module.exports = function logout(ws, data) {
-    console.log("LOGOUT EXECUTADO");
+const callconfigs = require("./config");
 
+module.exports = function inicServer(ws, data) {
     ws.send(JSON.stringify({
-        message: "quitgame"
+        message: "serverconnected"
     }));
 };
