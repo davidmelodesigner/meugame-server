e7f5cd99430433a4f93a55703bc27165148bd838
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-09 11:59:22 -0300

    Update home.js

--- home.js
+++ home.js
@@ -1,9 +1,5 @@
 
-const express = require("express");
-const http = require("http");
-const WebSocket = require("ws");
 
-const callconfigs = require("./config");
 
 module.exports = function homepage(ws, data) {
     ws.send(JSON.stringify({
