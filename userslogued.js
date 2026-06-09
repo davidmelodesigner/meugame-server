da97d721340f30c4f51eaaccc0fd42eef8ca6c1c
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-08 21:14:10 -0300

    Update userslogued.js

--- userslogued.js
+++ userslogued.js
@@ -1,1 +1,14 @@
 
+const { Pool } = require("pg");
+const callconfigs = require("./config");
+
+const pool = new Pool({
+    connectionString: callconfigs("postgre"),
+    ssl: { rejectUnauthorized: false }
+});
+
+module.exports = function userlogued(ws, data) {
+  ws.send(JSON.stringify({
+    "message":"userconected"
+  });
+};
