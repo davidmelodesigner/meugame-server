43583be587692158bfac333036a911ff3d432ea1
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-09 23:53:26 -0300

    Update playerupdate.js

--- playerupdate.js
+++ playerupdate.js
@@ -1,40 +1,10 @@
-const onlineUsers = new Map();
+module.exports = function inicServer(ws, data) {
 
-function registerUser(ws, userId) {
-    ws.userId = userId;
-    onlineUsers.set(userId, ws);
-}
+    console.log("CLIENT CONNECTED");
 
-function handlePlayerUpdate(ws, data) {
+    ws.send(JSON.stringify({
+        message: "serverconnected",
+        data:data
+    }));
 
-    const userId = data.userid;
-
-    if (!userId) {
-        console.log("PLAYERUPDATE SEM USERID");
-        return;
-    }
-
-    ws.userId = userId;
-    onlineUsers.set(userId, ws);
-
-    const payload = {
-        message: "playerupdate_echo",
-        userid: userId,
-        x: data.x,
-        y: data.y,
-        z: data.z,
-        rx: data.rx,
-        ry: data.ry,
-        rz: data.rz
-    };
-
-    console.log("ENVIANDO PRO UPBGE:", payload);
-
-    ws.send(JSON.stringify(payload));
-}
-
-module.exports = {
-    handlePlayerUpdate,
-    registerUser,
-    onlineUsers
 };
