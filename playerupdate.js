db904e39cdf44842390360184541aa42e6eabeac
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-10 10:59:38 -0300

    Update playerupdate.js

--- playerupdate.js
+++ playerupdate.js
@@ -13,7 +13,7 @@ module.exports = function playerupdate(ws, data, wss) {
 
     wss.clients.forEach(client => {
 
-        if (client.readyState === 1) {
+        if (client.readyState === 1 && client !== ws) {
             client.send(payload);
         }
 
