2eaac061f2b5566d37d43c361703cbb49558c59c
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-10 10:30:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const playerupdate = require("./playerupdate");
 
 const app = express();
 
-
 app.get("/", (req, res) => {
     homepage(req, res);
 });
@@ -23,8 +22,15 @@ const wss = new WebSocket.Server({ server });
 
 wss.on("connection", (ws) => {
 
+    ws.isAlive = true;
+
+    ws.on("pong", () => {
+        ws.isAlive = true;
+    });
+
     ws.on("message", (msg) => {
         const data = JSON.parse(msg.toString());
+
         if (data.message == "startserver") {
             inicServer(ws, data);
         }
@@ -42,18 +48,47 @@ wss.on("connection", (ws) => {
         }
 
         if (data.message === "quitserver") {
-            logout(ws, data);
-            
+            logout(ws, data, wss);
         }
+
         if (data.message === "playerupdate") {
-            playerupdate(ws, data,wss);
-            
+            playerupdate(ws, data, wss);
         }
     });
 
-    
-
+    ws.on("close", () => {
+        ws.isAlive = false;
+    });
 });
+
+setInterval(() => {
+
+    wss.clients.forEach(ws => {
+
+        if (ws.isAlive === false) {
+
+            if (ws.userId) {
+                console.log("TIMEOUT REMOVE:", ws.userId);
+
+                wss.clients.forEach(client => {
+                    if (client.readyState === 1) {
+                        client.send(JSON.stringify({
+                            message: "userdisconnect",
+                            userid: ws.userId
+                        }));
+                    }
+                });
+            }
+
+            return ws.terminate();
+        }
+
+        ws.isAlive = false;
+        ws.ping();
+
+    });
+
+}, 5000);
 
 server.listen(process.env.PORT || 3000, () => {
     console.log("Servidor online");
