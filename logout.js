8d6cf87f585760bbabf9533896ba85d5ff045e41
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-10 10:18:19 -0300

    Update logout.js

--- logout.js
+++ logout.js
@@ -1,6 +1,27 @@
+module.exports = function logout(ws, data, wss) {
 
-module.exports = function logout(ws, data) {
-    ws.send(JSON.stringify({
-        message: "quitgame"
-    }));
+    const userId = data.userid || ws.userId;
+
+    if (!userId) return;
+
+    console.log("USER DESCONECTOU:", userId);
+
+    ws.userId = null;
+
+    const payload = JSON.stringify({
+        message: "userdisconnect",
+        userid: userId
+    });
+
+    wss.clients.forEach(client => {
+
+        if (client.readyState === 1) {
+
+            if (client.userId === userId) {
+                client.userId = null;
+            }
+
+            client.send(payload);
+        }
+    });
 };
