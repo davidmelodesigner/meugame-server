1636022a2fcbcdcb74155964847afd715dafb73a
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-10 00:34:40 -0300

    Update playerupdate.js

--- playerupdate.js
+++ playerupdate.js
@@ -1,7 +1,22 @@
 module.exports = function inicServer(ws, data) {
 
     console.log("CLIENT CONNECTED");
+    const payload = JSON.stringify({
+        message: "playerupdate_echo",
+        userid: data.userid,
+        x: data.x,
+        y: data.y,
+        z: data.z,
+        rx: data.rx,
+        ry: data.ry,
+        rz: data.rz
+    });
 
+    wss.clients.forEach(client => {
+        if (client.readyState === 1) {
+            client.send(payload);
+        }
+    });
     ws.send(JSON.stringify({
         message: "playerupdate_echo",
         data:data
