18f7336b217aeaee573f49ad6a51d5e9f22790cc
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-11 02:24:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -41,12 +41,7 @@ wss.on("connection", (ws) => {
 
             ws.send(JSON.stringify({
                 message: "updateplayer",
-                data: JSON.stringify({
-                        x: ws.x,
-                        y: ws.y,
-                        z: ws.z,
-                        userId: ws.userId
-                    })
+                data: ws.data
             }));
         }
     });
