d4ac1312b1c19705c52ee79d2733b0ce5a905b81
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-11 01:35:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ wss.on("connection", (ws) => {
     ws.on("message", (msg) => {
 
         const data = JSON.parse(msg.toString());
-        if(data.message=="update"){
+        if(data.message=="startserver"){
             ws.send(JSON.stringify({
-                    message: "updateplayer",
+                    message: "connected",
                     data: data
             }));
 
