fd39fff346251a6552121ee28f81f8fdcfb680d3
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-11 01:46:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
 const homepage = require("./home.js");
+
 const app = express();
 
 app.get("/", (req, res) => {
@@ -21,15 +22,25 @@ wss.on("connection", (ws) => {
 
     ws.userId = generateId();
 
+    // 🔥 AGORA SIM: salva no players
+    players[ws.userId] = {
+        id: ws.userId
+    };
+
     ws.on("message", (msg) => {
         const data = JSON.parse(msg.toString());
 
         if (data.message === "startserver") {
+
             ws.send(JSON.stringify({
                 message: "connected",
                 id: ws.userId
             }));
         }
+    });
+
+    ws.on("close", () => {
+        delete players[ws.userId];
     });
 
 });
