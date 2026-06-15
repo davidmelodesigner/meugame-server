8e68015dbf555137534adc4e278060d5d9f8369f
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-14 23:24:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
 const homepage = require("./home.js");
+const { Pool } = require("pg");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const pool = new Pool({
+    connectionString: "postgresql://neondb_owner:npg_qUTQ3o4esZjF@ep-sparkling-pond-apv9ip8u-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 const app = express();
 
