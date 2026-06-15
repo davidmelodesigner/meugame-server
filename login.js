03932fb6015c1c8697b3f5a2cb01a62e9daca152
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-15 09:44:08 -0300

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 const evenconfig = require("./evenconfig.js");
 
 const pool = new Pool({
-    connectionString: "postgresql://neondb_owner:npg_qUTQ3o4esZjF@ep-sparkling-pond-apv9ip8u-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+    connectionString: evenconfig(),
     ssl: {
         rejectUnauthorized: false
     }
