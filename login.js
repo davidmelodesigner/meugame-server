de261054fdeafdf2d3bb9ea0b010d4a1892ce1b9
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-15 00:14:14 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString:"postgresql://neondb_owner:npg_qUTQ3o4esZjF@ep-sparkling-pond-apv9ip8u-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
     ssl: {
         rejectUnauthorized: false
     }
@@ -12,21 +12,20 @@ async function loginusers(usuario, senha) {
     try {
 
         const result = await pool.query(
-            "SELECT * FROM usuarios WHERE usuario = $1 AND senha = $2 LIMIT 1",
+            "SELECT id, usuario FROM usuarios WHERE usuario = $1 AND senha = $2 LIMIT 1",
             [usuario, senha]
         );
 
-        if (result.rows.length > 0) {
-
+        if (result.rows.length === 0) {
             return {
-                success: true,
-                usuario: result.rows[0]
+                success: false
             };
-
         }
 
         return {
-            success: false
+            success: true,
+            id: result.rows[0].id,
+            usuario: result.rows[0].usuario
         };
 
     } catch (err) {
@@ -34,8 +33,7 @@ async function loginusers(usuario, senha) {
         console.log(err);
 
         return {
-            success: false,
-            erro: err.message
+            success: false
         };
 
     }
