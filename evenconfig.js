2311aad82a1f132f7815561267d69f5fead3c255
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-15 09:58:41 -0300

    Update evenconfig.js

--- evenconfig.js
+++ evenconfig.js
@@ -1,5 +1,1 @@
-module.exports = function evenconfig() {
-
-    return "postgresql://neondb_owner:npg_qUTQ3o4esZjF@ep-sparkling-pond-apv9ip8u-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require";
-
-};
+module.exports = "postgresql://neondb_owner:npg_qUTQ3o4esZjF@ep-sparkling-pond-apv9ip8u-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require";
