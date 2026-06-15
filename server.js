507f8bf5af1aa64a98070d41c16571851971c00b
gamextore <54872302+davidmelodesigner@users.noreply.github.com>
2026-06-15 09:19:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -20,94 +20,17 @@ const users = {};
 // ---------------- CONNECTION ----------------
 wss.on("connection", (ws) => {
 
-    console.log("Cliente conectado.");
-
-    ws.send(JSON.stringify({
-        message: "connected"
-    }));
-
     ws.on("message", async (msg) => {
 
         try {
 
             const data = JSON.parse(msg.toString());
 
-            // ---------------- LOGIN ----------------
-            if (data.message === "login") {
-
-                const resultado = await loginusers(
-                    data.usuario,
-                    data.senha
-                );
-
-                if (resultado.success) {
-
-                    ws.userId = "user_" + resultado.id;
-
-                    users[ws.userId] = {
-                        id: ws.userId,
-                        nome: resultado.usuario,
-                        x: 200,
-                        y: 200,
-                        anim: null
-                    };
-
-                    ws.send(JSON.stringify({
-                        message: "login",
-                        success: true,
-                        id: ws.userId,
-                        nome: resultado.usuario
-                    }));
-
-                    enviarUsuarios();
-
-                } else {
-
-                    ws.send(JSON.stringify({
-                        message: "login",
+            if (data.message==="testconnection") {
+            	ws.send(JSON.stringify({
+                        message: "connected",
                         success: false
                     }));
-
-                }
-
-                return;
-            }
-            // ---------------- LOGOUT ----------------
-            if (data.message === "disconnecting") {
-            
-                if (!ws.userId) return;
-            
-                console.log("Logout solicitado:", ws.userId);
-            
-                // remove do sistema
-                delete users[ws.userId];
-            
-                // avisa o cliente que pode sair
-                ws.send(JSON.stringify({
-                    message: "disconnected",
-                    success: true
-                }));
-            
-                // fecha conexão
-                ws.close();
-            
-                // atualiza outros players
-                enviarUsuarios();
-            
-                return;
-            }
-            // ---------------- UPDATE PLAYER ----------------
-            if (data.message === "updateplayer") {
-
-                if (!users[ws.userId]) return;
-
-                users[ws.userId].x = data.x;
-                users[ws.userId].y = data.y;
-                users[ws.userId].anim = data.anim;
-
-                enviarUsuarios();
-
-                return;
             }
 
         } catch (err) {
